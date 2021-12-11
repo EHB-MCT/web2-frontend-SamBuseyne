7c17011cc5bc03899519bb07175f0a38ba87d744
SamBuseyne <sam.buseyne@student.ehb.be>
2021-12-11 16:52:45 +0100

    Updating code
    Adding and working out pages, also starting to getting data from API (OmdbAPI).

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,47 @@
-window.onload = function (){
+window.onload = function () {
     console.log("script loaded!")
+
+    let url = `http://www.omdbapi.com/?s=tenet&apikey=3d9f5461&`;
+
+
+
+    document.getElementById('shuffleButton').addEventListener('click', () => {
+        console.log("let'shuffle!")
+
+        fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                let movieData = data.Search;
+                let poster = document.getElementById('moviePoster1');
+                let htmlString = "";
+                htmlString += `
+            <div id="moviePoster1">
+            <figure>
+                <img src="${movieData[1].Poster}">
+            </figure>
+        </div>
+            `
+            poster.innerHTML = htmlString;
+            })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
